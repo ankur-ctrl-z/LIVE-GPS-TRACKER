ddba1c75353d71de6e78a549bd7fa14625c69a61
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-09 13:15:29 +0530

    Added project

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ const socket = io();
 const map = L.map("map").setView([20.5937, 78.9629], 5);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "© OpenStreetMap contributors",
+  attribution: "ankur's map",
 }).addTo(map);
 
 const otherMarkers = {};
