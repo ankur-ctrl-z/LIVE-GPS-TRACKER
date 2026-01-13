8da62bdf8a6330a735defc120287064fb1cb3ab6
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-13 20:32:26 +0530

    Icon change

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const path = require("path");
- 
-const socketio = require('socket.io');
-
 const server = http.createServer(app);
-const io = socketio(server);
+const io = new Server(server);
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname, "public")));
+/* Serve static files */
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
-  console.log("connected:", socket.id);
+  console.log("User connected:", socket.id);
 
-  socket.on("send-location", ({ latitude, longitude }) => {
+  socket.on("send-location", (data) => {
     socket.broadcast.emit("receive-location", {
       id: socket.id,
-      latitude,
-      longitude,
+      ...data,
     });
   });
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", socket.id);
+    io.emit("user-disconnected", socket.id);
+    console.log("User disconnected:", socket.id);
   });
 });
 
-app.get("/", (req,res) => {
-    res.render("index");
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
 });
 
-server.listen(3000);
