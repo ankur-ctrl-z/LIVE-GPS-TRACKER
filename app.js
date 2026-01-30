ee8be4a8c088a00af512dc5122546cd00df60299
ANKUR SHARMA <ankur143513@gmail.com>
2026-01-30 13:36:40 +0530

    fixed the dimentions of rider's icon

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", () => { 
     io.emit("user-disconnected", socket.id);
     console.log("User disconnected:", socket.id);
   });
